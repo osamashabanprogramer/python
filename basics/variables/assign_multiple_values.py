f274e7523347b43317bcd34ed39b87664d1413c3
osama shaban <osamashabanprogramer@gmail.com>
2021-03-09 11:41:05 +0200

    study from work

---
+++ basics/variables/assign_multiple_values.py
@@ -0,0 +1,23 @@
+# Many Values to Multiple Variables
+# Python allows you to assign values to multiple variables in one line:
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# One Value to Multiple Variables
+# And you can assign the same value to multiple variables in one line:
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+# Unpack a Collection
+# If you have a collection of values in a list, tuple etc.
+#  Python allows you extract the values into variables.
+#  This is called unpacking.
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
